a7a10b0677c04eb72c291708a8d14440165c6bec
Dan Muchiri <dan.muchiri@student.moringaschool.com>
2024-02-02 14:26:29 +0300

    Completed assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,9 +1,17 @@
 // Code Keypad Component Here
+import React from "react";
 
-function Keypad (){
-    return (
-        <div></div>
-    )
+function Keypad() {
+  function handlePassword() {
+    console.log('Entering password...');
+  }
+
+  return (
+    <div>
+      <input onChange={handlePassword} type="password" placeholder="Enter password" />
+      <button>ME</button>
+    </div>
+  );
 }
 
 export default Keypad;
